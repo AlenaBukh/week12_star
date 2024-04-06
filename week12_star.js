549fa2d100c339121901ffeada90306587af23ed
AlenaBukh <alena89xxx@gmail.com>
2024-04-06 18:11:09 +0300

    initial commit

---
+++ week12_star.js
@@ -0,0 +1,20 @@
+function validatedName(value) {
+  return value[0].toUpperCase() + value.substring(1).toLowerCase();
+}
+
+function checkSpam(str) {
+  return str.replaceAll(/viagra/gi, "***");
+}
+
+function sendComment() {
+  const newName = document.getElementById("name").value;
+  const newUrl = document.getElementById("url").value;
+  const newComment = document.getElementById("comment").value;
+
+  const validatedName = validatedName(newName);
+  const validatedComment = checkSpam(newComment);
+
+  document.getElementById(
+    "saidComment"
+  ).innerHTML = `${validatedName} <img src=${newUrl} /> ${validatedComment}`;
+}
